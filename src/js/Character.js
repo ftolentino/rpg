f2e9a0cd675fb6991715733b3c0a4c489a2ee624
ftolentino <filmer.tolentino@gmail.com>
2022-06-23 11:39:17 -0700

    organize project structure
    Co-authored-by: Kyle Chinick <kylechinick@icloud.com>
    Co-authored-by: matt wilkinson <wilkinsonmatt01@gmail.com>

---
+++ src/js/Character.js
@@ -0,0 +1,11 @@
+export default class Character {
+  constructor(classType, hp, armor) {
+    this.classType = classType;
+    this.hp = hp;
+    this.armor = armor;
+  }
+
+  testFunction() {
+    return 'hello';
+  }
+}
