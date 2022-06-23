2331f88debb704ce6b720d0eeb716ac252e329fe
ftolentino <filmer.tolentino@gmail.com>
2022-06-23 15:31:20 -0700

    Add successful test for Ranger instance
    Co-authored-by: Matt Wilkinson <wilkinsonmatt@users.noreply.github.com>
    Co-authored-by: Kyle Chinick <kylechinick@users.noreply.github.com>

--- __tests__/character.tests.js
+++ __tests__/character.tests.js
@@ -1,4 +1,5 @@
-import Character from '../src/js/Character';
+import {Character} from '../src/js/Character';
+import {Ranger} from '../src/js/Character';
 
 describe('Character', () => { 
   
@@ -12,5 +13,19 @@ describe('Character', () => {
     const character1 = new Character('Brick', 9001, 'chartrusse');
     expect(character1.charAttributes()).toEqual(['Brick', 9001, 'chartrusse']);
   });
+
+  test('it should create a Ranger class instance with name, hp, color, and bowType properties', () => {
+    const character2 = new Ranger('Ranger Brick', 3, 'Brown', 'Long Bow');
+    expect(character2)
+    .toEqual({
+        name: "Ranger Brick", 
+        hp: 3, color: "Brown", 
+        bowType: "Long Bow"
+      });
+  });
 });
 
+// name: 'Ranger Brick';
+// hp: 3;
+// color: 'Brown';
+// bowType: 'Long Bow';
--- src/js/Character.js
+++ src/js/Character.js
@@ -1,4 +1,4 @@
-export default class Character {
+export class Character {
   constructor(name, hp, color) {
     this.name = name;
     this.hp = hp;
@@ -9,3 +9,21 @@ export default class Character {
     return [this.name, this.hp, this.color];
   }
 }
+
+export class Ranger extends Character {
+  constructor(name, hp, color, bowType) {
+    super(name, hp, color);
+    this.bowType = bowType;
+  }
+  longRangeAttack() {
+    return `you've been attacked, yo`;
+  }
+}
+
+
+// const character1 = new Character('Brick', 9001, 'chartrusse');
+
+// const character2 = new Ranger('Ranger Brick', 3, 'Brown', 'Long Bow');
+
+// console.log(character1);
+// console.log(character2);
